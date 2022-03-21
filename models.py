4cb27bd7122b8f35ce581ade405af7beb5fd5833
Huwwww <lmbrohw@gmail.com>
2022-03-21 22:27:05 +0800

    add models.py

---
+++ models.py
@@ -0,0 +1,163 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels):
+        super().__init__()
+
+        conv_block = [
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(in_channels, in_channels, kernel_size=3),
+            nn.InstanceNorm2d(in_channels),
+            nn.ReLU(inplace=True),
+            nn.ReflectionPad2d(1),
+            nn.Conv2d(in_channels, in_channels, kernel_size=3),
+            nn.InstanceNorm2d(in_channels)
+        ]
+
+        self.model = nn.Sequential(*conv_block)
+
+    def forward(self, x):
+        return x + self.model(x)
+
+
+class Generator(nn.Module):
+    def __init__(self, in_channels=3, out_channels=3, n_residual_blocks=9):  # 默认需要9个残差块
+        super().__init__()
+
+        # 初始化卷积快
+        model = [
+            nn.ReflectionPad2d(3),
+            nn.Conv2d(in_channels, 64, kernel_size=7),
+            nn.InstanceNorm2d(64),
+            nn.ReLU(inplace=True)
+        ]
+
+        # 编码
+        model += [
+            nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1),
+            nn.InstanceNorm2d(128),
+            nn.ReLU(inplace=True)
+        ]
+        model += [
+            nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1),
+            nn.InstanceNorm2d(256),
+            nn.ReLU(inplace=True)
+        ]
+
+        # 加入残差块
+        for _ in range(n_residual_blocks):
+            model += [ResidualBlock(256)]
+
+        # 解码
+        model += [
+            nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.InstanceNorm2d(128),
+            nn.ReLU(inplace=True)
+        ]
+        model += [
+            nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.InstanceNorm2d(64),
+            nn.ReLU(inplace=True)
+        ]
+
+        # 输出
+        model += [
+            nn.ReflectionPad2d(3),
+            nn.Conv2d(64, out_channels, kernel_size=7),
+            nn.Tanh()
+        ]
+
+        self.model = nn.Sequential(*model)
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class Discriminator(nn.Module):
+    def __init__(self, in_channels=3):
+        super().__init__()
+
+        # 卷积块
+        model = [
+            nn.Conv2d(in_channels, 64, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2, inplace=True)
+        ]
+
+        model += [
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
+            nn.InstanceNorm2d(128),
+            nn.LeakyReLU(0.2, inplace=True)
+        ]
+
+        model += [
+            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),
+            nn.InstanceNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True)
+        ]
+
+        model += [
+            nn.Conv2d(256, 512, kernel_size=4, stride=1, padding=1),
+            nn.InstanceNorm2d(512),
+            nn.LeakyReLU(0.2, inplace=True)
+        ]
+
+        # FCN 分类
+        model += [
+            nn.Conv2d(512, 1, kernel_size=4, padding=1)
+        ]
+
+        self.model = nn.Sequential(*model)
+
+    def forward(self, x):
+        x = self.model(x)
+        # 均值池化（下采样）+ flatten
+        return F.avg_pool2d(x, x.size()[2:]).view(x.size()[0], -1)
+
+
+class Attention(nn.Module):
+    def __init__(self, in_channels=3):
+        super.__init__()
+
+        model = [
+            nn.Conv2d(in_channels, 32, kernel_size=7, stride=1, padding=3),
+            nn.InstanceNorm2d(32),
+            nn.ReLU(inplace=True)
+        ]
+
+        model += [
+            nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1),
+            nn.InstanceNorm2d(64),
+            nn.ReLU(inplace=True)
+        ]
+
+        model += [
+            ResidualBlock(64)
+        ]
+
+        model += [
+            nn.UpsamplingNearest2d(scale_factor=2)
+        ]
+
+        model += [
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.InstanceNorm2d(64),
+            nn.ReLU(inplace=True)
+        ]
+
+        model += [
+            nn.Conv2d(64, 32, kernel_size=3, stride=1, padding=1),
+            nn.InstanceNorm2d(32),
+            nn.ReLU(inplace=True)
+        ]
+
+        model += [
+            nn.Conv2d(32, 1, kernel_size=7, stride=1, padding=3),
+            nn.Sigmoid()
+        ]
+
+        self.model = nn.Sequential(*model)
+
+    def forward(self, x):
+        return self.model(x)
